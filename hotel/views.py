9c28b532e5b31b1d6e91a4d7dea886265b431d69
shubhamjain31 <sj27754@gmail.com>
2022-11-04 22:41:10 +0530

    some changes in views

--- hotel/views.py
+++ hotel/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 from .models import EventAttendees, Event, Announcement
+from employee.models import Employee
 
 # Create your views here.
 
